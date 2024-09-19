241a3ccf085321632066abadbb0dc26de0366dcf
V-Premnath <114846770+V-Premnath@users.noreply.github.com>
2024-09-19 23:11:57 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,59 +1,117 @@
 import React, { useState } from 'react';
 import { Avatar, Button, TextField, Link, Box, Typography, Container } from '@mui/material';
-
 import { useFormik } from "formik";
-import CircularProgress, { circularProgressClasses } from "@mui/material/CircularProgress";
-import { setDefaultResultOrder } from 'dns';
-export default function  Login(): React.JSX.Element {
-  
-  const [loader,setLoader] = useState(false);
-  const [error,setError] =  useState("");
+import CircularProgress from "@mui/material/CircularProgress";
+import { useNavigate } from "react-router-dom";
 
-  interface FormValues{
-    email:string;
-    password:string;
+export default function Login({setLogin}:{setLogin:React.Dispatch<React.SetStateAction<boolean>>} ): React.JSX.Element {
+
+  const navigate = useNavigate()
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState("");
+  const [emailValid, setEmailValid] = useState(false)
+  const [passwordValid, setPasswordValid] = useState(false)
+  interface FormValues {
+    email: string;
+    password: string;
   };
 
-  const initialValues:FormValues={
-    email:"",
-    password:""
+  const initialValues: FormValues = {
+    email: "",
+    password: ""
   };
 
-  const validate = (values:FormValues)=> {
+  const validate = (values: FormValues) => {
 
     const errors: Partial<FormValues> = {};
     const emailPattern = /^[a-zA-Z0-9._%+-]+@rguktrkv\.ac\.in$/;
+    const passwordLength = 8;
+
     if (!emailPattern.test(values.email)) {
-      errors.email = "Invalid email";
-    } 
-    else {
-      errors.email = "";
+      console.log("email error")
+      setEmailValid(true)
+      setError("*Invalid Email Address")
+      errors.email = ""
     }
-    return errors; 
+    else {
+      errors.email = "true";
+      console.log("no email error")
+      setEmailValid(false)
+      setError("")
+    }
+    if (errors.email !== "") {
+      if (passwordLength <= values.password.length) {
+        setPasswordValid(false)
+        setError("")
+      }
+      else {
+        setPasswordValid(true)
+        setError("*Password must be atleast 8 characters")
+      }
+    }
+    return errors;
   }
 
-  const formik = useFormik <FormValues>({
+  const formik = useFormik<FormValues>({
     initialValues,
     validate,
-    onSubmit:(values)=>{}
+    onSubmit: (values) => { }
   });
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setLoader(true);
-    try{
-    // send an api request
-    //  const response= await fetch("",{
-    //    method : 'post',
-    //    headers : {
-    //      "Content-Type" : "application/json",
-    //    },
-    //  });
+
+    try {
+      // send an api request
+      //  const response= await fetch("",{
+      //    method : 'post',
+      //    headers : {
+      //      "Content-Type" : "application/json",
+      //    },
+      //  });
+      if(formik.values.email === "admin@rguktrkv.ac.in" && formik.values.password === "Admin@123"){
+        setLogin(true);
+        navigate("/XLSX")
+      }
+      else{setError("*Wrong Credentials")}
     }
-    catch(e){
+    catch (e) {
       setError("error");
     }
   };
+
+
+  const inputs: React.JSX.Element = (
+    <>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        type="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        color={emailValid ? "error" : "primary"}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        color={passwordValid ? "error" : "primary"}
+      />
+    </>
+  )
 
   return (
     <Container component="main" maxWidth="xs">
@@ -65,45 +123,25 @@ export default function  Login(): React.JSX.Element {
           alignItems: 'center',
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: 'primary',height:50,width:50 }}>
+        <Avatar sx={{ m: 1, bgcolor: 'primary', height: 50, width: 50 }}>
         </Avatar>
         <Typography component="h1" variant="h5">
           Login
         </Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 0.5 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          
+          {inputs}
           <Button
             type="submit"
             fullWidth
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
-            
+
           >
-          {loader ? (
-                <CircularProgress size={27} sx={{ color: "white" }} />
-              ) : (
-                "Login"
-              )}
+            {loader ? (
+              <CircularProgress size={27} sx={{ color: "white" }} />
+            ) : (
+              "Login"
+            )}
           </Button>
           <Box >
             <Box >
@@ -112,10 +150,10 @@ export default function  Login(): React.JSX.Element {
               </Link>
             </Box>
             {error && (
-          <Typography color="error" style={{ marginTop: 16, textAlign: 'center' }}>
-            {error}
-          </Typography>
-        )}
+              <Typography color="error" style={{ marginTop: 16, textAlign: 'left' }}>
+                {error}
+              </Typography>
+            )}
           </Box>
         </Box>
       </Box>
