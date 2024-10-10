a8f59f9d07f40a005f08617fab7ed44419b8a672
GeethaReddy222 <geethareddy4815@gmail.com>
2024-10-10 14:13:21 +0530

    Add Engg_Layout_2

--- src/certificateLayouts/Engg_Layout_2.tsx
+++ src/certificateLayouts/Engg_Layout_2.tsx
@@ -1,3 +1,4 @@
+//Engg layout
 import React, { useState, useEffect } from "react";
 import MerriweatherRegular from "../fonts/Merriweather-Regular.ttf";
 import MerriweatherLight from "../fonts/Merriweather-Light.ttf";
