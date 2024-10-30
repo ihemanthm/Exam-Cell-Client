ee70fe84d544d7c3ef4f7345c694f0b21a2ce76b
GeethaReddy222 <geethareddy4815@gmail.com>
2024-10-30 07:54:32 +0530

    puc grade calculation

--- src/components/ResultsPage.tsx
+++ src/components/ResultsPage.tsx
@@ -52,17 +52,26 @@ export default function ResultsPage() {
             variant: "info",
           })
         );
-      } catch (error: any) {
-        dispatch(
-          setSnackBar({
-            message: "Failed to fetch student details",
-            variant: "error",
-          })
-        );
-      } finally {
         setLoader(false);
+      } catch (error: any) {
+        setLoader(false);
+        if (error.status === 404) {
+          dispatch(
+            setSnackBar({
+              message: "User not found",
+              variant: "warning",
+            })
+          );
+        } else {
+          dispatch(
+            setSnackBar({
+              message: "Failed to search student",
+              variant: "error",
+            })
+          );
+        }
       }
-    },
+    }
   });
 
   return (
@@ -169,3 +178,4 @@ export default function ResultsPage() {
     </>
   );
 }
+ 
